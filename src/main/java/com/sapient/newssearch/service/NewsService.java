358030401740f4f08f338f01d70363054a823b21
avinashharidasu <avinash.haridasu@gmail.com>
2025-09-25 00:54:10 +0530

    added react based front end application

--- src/main/java/com/sapient/newssearch/service/NewsService.java
+++ src/main/java/com/sapient/newssearch/service/NewsService.java
@@ -8,6 +8,7 @@ import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +22,7 @@ public class NewsService {
 
     private final NewsClient client;
     private final NewsSearchResponseMapper mapper;
+    private final ReactiveRedisTemplate<String, Object> template;
 
     @Retry(name = NEWS_RESULT_RETRY, fallbackMethod = "defaultNewsResults")
     @Cacheable(value = NEWS_RESULT_CACHE, keyGenerator = "newsCacheKeyGenerator")
@@ -35,7 +37,7 @@ public class NewsService {
         log.error("Retry policy newsSearchRetryPolicy triggered due to exception {}", ex.getMessage());
 
         var fallback = new NewsSearchResponseDto();
-        fallback.setStatus("fallback");
+        fallback.setStatus("No results found");
         fallback.setTotalArticles(0);
         fallback.setArticles(java.util.Collections.emptyList());
         return Mono.just(fallback);
